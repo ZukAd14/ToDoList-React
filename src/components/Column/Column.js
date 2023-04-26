88dac97b5ef1cf5b2865c0fa5798e0fd2cf75181
Adam <adam.zukowski471@gmail.com>
2023-04-26 17:11:27 +0200

    Add redux store for columns and cards. Task 17.2

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -1,18 +1,26 @@
 import styles from  './Column.module.scss';
+import { useSelector } from 'react-redux';
 import Card from '../Card/Card';
 import CardForm from '../CardForm/CardForm';
 
 
-const Column = props => {
+const Column = (props) => {
+
+    const cards = useSelector(state => state.cards.filter(card => card.columnId === props.id));
+
     return (
         <article className={styles.column}>
             <h2 className={styles.title} title={props.title}>
                 <span className={styles.icon + ' fa fa-' + props.icon}></span>{props.title}
             </h2>
             <ul className={styles.cards}>
-                {props.cards.map(card => <Card key={card.id} title={card.title} />)}
+                {cards.map(card => 
+                    <Card 
+                        key={card.id} 
+                        {...card} />
+                )}
             </ul>
-            <CardForm columnId={props.id} action={props.action} />
+            <CardForm />
         </article>
     );
 };
